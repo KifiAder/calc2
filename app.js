5e3cec19632193b7f007095717d9b097ccd748ea
KifiAder <141772572+KifiAder@users.noreply.github.com>
2024-11-12 16:21:13 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var n = "";
+function func() {
+  //var num1 = Number(document.getElementById("num1").value);
+  //console.log(num1)
+  var result = eval(n);
+  document.getElementById("result").innerHTML = result;
+  n = ""
+}
